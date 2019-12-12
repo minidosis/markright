c82dfe2f9d59207a229c4e05a2f52d41856dcd82
Pau Fernández <pau.fernandez@upc.edu>
2019-12-12 12:01:11 +0100

    Corrijo el informal-test
    - Aunque esto no sé si va a sobrevivir.

--- examples/informal-test.js
+++ examples/informal-test.js
@@ -1,10 +1,6 @@
 
 const fs = require('fs')
-const markright = require('../markright')
+const mr = require('../markright')
 
-const obj1 = markright.parse(fs.readFileSync('informal-test.mr').toString())
-// console.log(JSON.stringify(obj1, null, 2))
-const str1 = markright.stringify(obj1)
-console.log(str1)
-const obj2 = markright.parse(str1)
-console.log(markright.stringify(obj2))
+const obj1 = mr.parseFileRecur('informal-test.mr')
+console.log(JSON.stringify(obj1, null, 2))
