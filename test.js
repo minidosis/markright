0cd9d17b44dad853c3247420ed35e644832b9974
Pau Fernández <pau.fernandez@upc.edu>
2019-12-12 12:00:48 +0100

    Carpeta de tests
    - Se ejecutan todos los tests de dentro de la carpeta 'tests'.
    - Btw, dejamos atrás el tema de ayer...

--- test.js
+++ test.js
@@ -1,39 +1,40 @@
 
+const fs = require('fs')
 const MR = require('./markright')
 const colors = require('colors');
 
 const write = (...args) => process.stdout.write(...args)
 
-const performTest = (title, input, output) => {
-  const print = (x) => {
-    switch (x.constructor) {
-      case String:
-        return x
-      case Array:
-        return `${x.map(print).join('')}`
-      case MR.Line:
-        return `Line(${x.children ? x.children.map(print).join('') : ''})`
-      case MR.BlockCommand: {
-        let result = `@${x.name}`
-        if (x.args) result += `(${x.args.join(',')})`
-        if (x.children) {
-          result += `\`${print(x.children)}\``
-        }
-        return result
+const print = (x) => {
+  switch (x.constructor) {
+    case String:
+      return x
+    case Array:
+      return `${x.map(print).join('')}`
+    case MR.Line:
+      return `Line(${x.children ? x.children.map(print).join('') : ''})`
+    case MR.BlockCommand: {
+      let result = `@${x.name}`
+      if (x.args) result += `(${x.args.join(',')})`
+      if (x.children) {
+        result += `\`${print(x.children)}\``
       }
-      case MR.InlineCommand: {
-        let result = `@${x.name}`
-        if (x.args) result += `(${x.args.join(',')})`
-        if (x.children) {
-          result += `${x.delim.open}${print(x.children)}${x.delim.close}`
-        }
-        return result
-      }
-      default:
-        throw new Error(`Unexpected object of type ${x.constructor}`)
+      return result
     }
+    case MR.InlineCommand: {
+      let result = `@${x.name}`
+      if (x.args) result += `(${x.args.join(',')})`
+      if (x.children) {
+        result += `${x.delim.open}${print(x.children)}${x.delim.close}`
+      }
+      return result
+    }
+    default:
+      throw new Error(`Unexpected object of type ${x.constructor}`)
   }
+}
 
+const performTest = (title, input, output) => {
   try {
     const parsed = MR.parseRecur(input)
     const actual = parsed.map(print).join('')
@@ -55,30 +56,42 @@ const performTest = (title, input, output) => {
   }
 }
 
-let testfile = process.argv[2]
-if (testfile === undefined) {
-  testfile = 'tests.mr'
+const processTestFile = (testfile) => {
+  let errors = []
+
+  MR.parseFile(testfile, {
+    'test': (args, testBody) => {
+      const testName = args[0]
+      let input, output;
+      MR.parse(testBody, {
+        'input': (_, rawChildren) => input = rawChildren,
+        'output': (_, rawChildren) => output = rawChildren,
+      })
+      if (!input || !output) {
+        throw new Error(`Error in test "${testName}": Input or output is empty!`)
+      }
+      const errs = performTest(testName, input, output)
+      if (errs) errors.push(...errs)
+    }
+  })
+
+  write('\n')
+  if (errors.length > 0) {
+    write(`\n${errors.join('\n')}\n`)
+  }
 }
 
-let errors = []
-
-MR.parseFile(testfile, {
-  'test': (args, testBody) => {
-    const testName = args[0]
-    let input, output;
-    MR.parse(testBody, {
-      'input': (_, rawChildren) => input = rawChildren,
-      'output': (_, rawChildren) => output = rawChildren,
-    })
-    if (!input || !output) {
-      throw new Error(`Error in test "${testName}": Input or output is empty!`)
-    }
-    const errs = performTest(testName, input, output)
-    if (errs) errors.push(...errs)
+// Process all files ending in '.mr' in the 'test' directory
+fs.readdir('./tests', (err, files) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
   }
+  const testfiles = files.filter(f => f.endsWith('.mr'))
+  const maxLength = testfiles.reduce((mx, f) => Math.max(mx, f.length), 0)
+  testfiles.forEach((testfile) => {
+    write(`${testfile}${' '.repeat(maxLength + 1 - testfile.length)}`)
+    processTestFile(`tests/${testfile}`)
+  })
 })
 
-write('\n')
-if (errors.length > 0) {
-  write(`\n${errors.join('\n')}\n`)
-}
