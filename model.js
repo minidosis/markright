06e9e3708a715e584532b2695fc5bfbe00d98e7d
Pau Fernández <pau.fernandez@upc.edu>
2019-12-15 12:09:56 +0100

    Separo en ficheros

---
+++ model.js
@@ -0,0 +1,94 @@
+
+class Item {
+  addRaw(str) {
+    if (this.rawChildren === undefined) {
+      this.rawChildren = ''
+    }
+    this.rawChildren += str + '\n'
+  }
+  hasRawChildren() { return this.rawChildren !== undefined }
+  add(str) { this.children = [...(this.children || []), str] }
+  toJson() { throw new Error(`Item.toJson is abstract! (obj = ${JSON.stringify(this)})`) }
+}
+
+class Text extends Item {
+  constructor(text) {
+    super()
+    this.text = text
+  }
+  toJson() { return `"${this.text}"` }
+}
+
+class _List extends Item {
+  constructor(children) {
+    super()
+    if (children) this.children = children
+  }
+  toJson() {
+    return `[${this.children ? this.children.map(x => x.toJson()).join(',') : ''}]`
+  }
+}
+
+class Block extends _List {
+}
+
+class Line extends _List { // = List<InlineItem>
+  add(item) { this.children = [...(this.children || []), item] }
+
+  isSingle() { return this.children && this.children.length === 1 }
+  isSingleCommand() { return this.isSingle() && this.children[0] instanceof Command }
+  isSingleBlockCommand() { return this.isSingle() && this.children[0] instanceof BlockCommand }
+
+  allCommandsToInlineCommands() {
+    this.children = this.children.map(item => {
+      return item instanceof Command ? item.toInlineCommand() : item
+    })
+  }
+
+  executeAllCommands(execFunc) {
+    if (this.children) {
+      this.children = this.children.map(item => {
+        return (item instanceof Command ? execFunc(item) : item)
+      })
+    }
+  }
+}
+
+class Command extends Item {
+  constructor(name, args) {
+    super()
+    if (name) this.name = name
+    if (args) this.args = args
+  }
+  toJson() {
+    let json = `{"cmd":"${this.name}"`
+    if (this.args) json += `,"args":[${this.args.map(x => `"${x}"`).join(',')}]`
+    if (this.delim) json += `,"delim":{"open":"${this.delim.open}","close":"${this.delim.close}"}`
+    if (this.children) json += `,"children":${this.children.toJson()}`
+    json += `}`
+    return json
+  }
+}
+
+class BlockCommand extends Command {
+  constructor(name, args, children) {
+    super(name, args)
+    if (children) this.children = children
+  }
+  toInlineCommand() {
+    return new InlineCommand(this.name, this.args)
+  }
+}
+
+class InlineCommand extends Command {
+  constructor(name, args, rawChildren, delim) {
+    super(name, args)
+    if (rawChildren) this.rawChildren = rawChildren
+    if (delim) this.delim = delim
+  }
+  toInlineCommand() { return this }
+}
+
+module.exports = {
+  Item, Text, Block, Line, Command, BlockCommand, InlineCommand,
+}
---
+++ stringify.js
@@ -0,0 +1,78 @@
+const stringify = (mr) => {
+  let indent = 0
+  let line = ''
+  let result = ''
+
+  const add = (x) => line += x
+  const newLine = () => {
+    result += line + '\n'
+    line = ' '.repeat(indent * 2)
+  }
+
+  const _stringify = (x) => {
+    if (x === null || x === undefined) {
+      return '';
+    }
+    switch (x.constructor) {
+      case Text: {
+        if (x.text === '@') add('@@')
+        else add(x.text)
+        break
+      }
+      case Block: {
+        x.children.forEach(item => {
+          newLine()
+          _stringify(item)
+        })
+        break
+      }
+      case Line: {
+        if (x.children) x.children.map(_stringify)
+        break
+      }
+      case BlockCommand: {
+        const { name, args, rawChildren, children } = x
+        add(`@${name}`)
+        if (args) add(`(${args.join(',')})`)
+        indent++
+        if (rawChildren) {
+          splitLines(rawChildren).forEach(line => {
+            newLine()
+            add(line)
+          })
+        } else {
+          _stringify(children)
+        }
+        indent--
+        break
+      }
+      case InlineCommand: {
+        const { name, args, delim, rawChildren, children } = x
+        add(`@${name}`)
+        if (args) add(`(${args.join(',')})`)
+        if (delim) {
+          add(delim.open)
+          if (rawChildren) {
+            add(rawChildren)
+          } else {
+            _stringify(children)
+          }
+          add(delim.close)
+        }
+        break
+      }
+      default:
+        throw new Error(`stringify of an unknown type! (obj = ${JSON.stringify(x)})`)
+    }
+  }
+
+  _stringify(mr)
+  if (line) {
+    result += line + '\n'
+  }
+  return result
+}
+
+module.exports = {
+  stringify
+}
