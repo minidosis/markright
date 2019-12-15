694b973c39166b3f7c8a45e2b2d0f7c34efbecbc
Pau Fernández <pau.fernandez@upc.edu>
2019-12-15 11:35:22 +0100

    Simplifico el ejemplo

--- examples/simple.js
+++ examples/simple.js
@@ -9,14 +9,12 @@ const mr = `
 This is @em<normal> text, with a @a(http://pauek.info){Link@x@y<>zzz}
 and more on the @b{next} line, @yay @yay2
 
-@done{true}
+@done{it is done}
 `
 
 const obj = markright.parse(mr, {
   em: (_, children) => new markright.Text(`**${children}**`),
-  done: (_, children) => {
-    console.log(children)
-    return markright.parse(`@parsedDone\n  new stuff\n`)
-  }
+  done: (_, children) => markright.parse(`@parsedDone\n  new stuff @newcmd{${children}} and more\n`),
+  yay: (_, children) => new markright.Text(`YAY`)
 })
 console.log(markright.stringify(obj))
